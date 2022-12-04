f3dad41ec9224c9c3c619992c8a9a9ce59a87230
wissal18 <lamouchi7wissal@gmail.com>
2022-12-04 18:42:28 +0100

    first commit

---
+++ Freelance_Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Freelance_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
